146196dc1ea6c4d7442352eb3792afbe93714f21
Kevin Özkaraca <80677798+kevinozkaraca@users.noreply.github.com>
2023-01-09 13:26:45 +0300

    affichage de la page index

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,34 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  // Récupération des données du fichier JSON dans des variables
+  const { city, country, id, name, portrait, price, tagline } = data;
+  const picture = `assets/photographers/${portrait}`;
 
-    const picture = `assets/photographers/${portrait}`;
-
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    // Création des éléments pour l'affichage des photographes
+    const div = document.createElement("div");
+    const link = document.createElement("a");
+    const article = document.createElement("article");
+    const imgElement = document.createElement("img");
+    const h2 = document.createElement("h2");
+    const locationElement = document.createElement("p");
+    const priceElement = document.createElement("p");
+    const taglineElement = document.createElement("p");
+    imgElement.setAttribute("src", picture);
+    imgElement.alt = `Photo du photographe ${name}`;
+    link.href = `./photographer.html?id=${id}`;
+    link.ariaLabel = name;
+    h2.textContent = name;
+    div.appendChild(locationElement);
+    div.appendChild(taglineElement);
+    div.appendChild(priceElement);
+    article.appendChild(div);
+    article.appendChild(link);
+    link.appendChild(imgElement);
+    link.appendChild(h2);
+    locationElement.textContent = `${city}, ${country}`;
+    taglineElement.textContent = tagline;
+    priceElement.textContent = `${price} €/jour`;
+    return article;
+  }
+  return { city, country, id, name, portrait, price, tagline, getUserCardDOM };
 }
