6417243aa89a82048100de6e87a750c64f06bef2
Kevin Özkaraca <80677798+kevinozkaraca@users.noreply.github.com>
2023-01-10 04:11:13 +0300

    page photographe et JS

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,16 @@
-//Mettre le code JavaScript lié à la page photographer.html
+// Récupération des informations du lien de la page :
+const params = new URLSearchParams(window.location.search);
+const photographerId = params.get("id");
+
+// Séléction du photographe :
+function fetchPhotographer(photographerId) {
+  fetch("./data/photographers.json").then(function (response) {
+    response.json().then(function (json) {
+      const result = json.photographers.find((data) => data.id == photographerId);
+      const { city, country, id, name, portrait, price, tagline } = result;
+      console.log(city, country, id, name, portrait, price, tagline);
+    });
+  });
+}
+
+fetchPhotographer(photographerId);
