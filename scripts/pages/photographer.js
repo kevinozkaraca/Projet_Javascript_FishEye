3425492769d4ef3a7d3e5749ce887a0282b41898
Kevin Özkaraca <80677798+kevinozkaraca@users.noreply.github.com>
2023-01-10 20:50:37 +0300

    integrer les infos de photographe

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -2,13 +2,32 @@
 const params = new URLSearchParams(window.location.search);
 const photographerId = params.get("id");
 
-// Séléction du photographe :
+// Séléction du photographe et affichage de son  :
 function fetchPhotographer(photographerId) {
   fetch("./data/photographers.json").then(function (response) {
     response.json().then(function (json) {
-      const result = json.photographers.find((data) => data.id == photographerId);
-      const { city, country, id, name, portrait, price, tagline } = result;
-      console.log(city, country, id, name, portrait, price, tagline);
+      console.log(json.media[0]);
+      // Data du photographe
+      const dataPhotographers = json.photographers.find((data) => data.id == photographerId);
+      const { city, country, id, name, portrait, price, tagline } = dataPhotographers;
+      // Medias du photographe
+      const dataMedias = json.media;
+      // Affichage des médias du photographe
+      for (let i = 0; i < dataMedias.length; i++) {
+        if (dataPhotographers.id == dataMedias[i].photographerId) {
+          // A integrer
+          console.log(
+            number,
+            dataMedias[i].id,
+            dataMedias[i].photographerId,
+            dataMedias[i].title,
+            dataMedias[i].image,
+            dataMedias[i].likes,
+            dataMedias[i].date,
+            dataMedias[i].price
+          );
+        }
+      }
     });
   });
 }
