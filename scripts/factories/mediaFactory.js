f91fbe5d151a8707a6d92148ee28cb66123f6e0a
Kevin Özkaraca <80677798+kevinozkaraca@users.noreply.github.com>
2023-01-11 19:13:40 +0300

    Afficher les images dans la boucle Media

--- scripts/factories/mediaFactory.js
+++ scripts/factories/mediaFactory.js
@@ -3,26 +3,24 @@ function mediaFactory(data) {
   const { id, photographerId, title, likes, date, price } = data;
   const imageUrl = `assets/${data.image}`;
   const videoUrl = `assets/${data.video}`;
-
   function getMediaCardDOM() {
     // Création des éléments pour l'affichage des médias
     const article = document.createElement("article");
     const link = document.createElement("a");
-    const imgElement = document.createElement("img");
     const div1 = document.createElement("div");
     const div2 = document.createElement("div");
     const h2 = document.createElement("h2");
     const p = document.createElement("p");
     // Injection des éléments dans le DOM
-    div1.className = "text_photo";
-    div2.className = "likes_photo";
-    h2.textContent = title;
-    p.textContent = likes;
     article.appendChild(link);
     article.appendChild(div1);
     div1.appendChild(div2);
     div1.appendChild(h2);
     div2.appendChild(p);
+    div1.className = "text_photo";
+    div2.className = "likes_photo";
+    h2.textContent = title;
+    p.textContent = likes;
     return article;
   }
   return { image: imageUrl, video: videoUrl, id, photographerId, title, likes, date, price, getMediaCardDOM };
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -9,59 +9,22 @@ async function getMedia() {
   const data = response.json();
   return data;
 }
-
+// Récupération des élément du DOM et boucle
 async function displayData(medias) {
   const photographersHeader = document.querySelector(".photograph-header");
-  medias.forEach((media) => {
-    const mediaModel = mediaFactory(media);
-    const userMediaDom = mediaModel.getMediaCardDOM();
-    console.log(medias.length);
-    for (let i = 0; i < medias.length; i++) {
-      if (+photographerId == medias[i].photographerId) {
-        photographersHeader.appendChild(userMediaDom);
-      }
-    }
-  });
-}
 
+  for (let i = 0; i < medias.length; i++) {
+    if (+photographerId == medias[i].photographerId) {
+      const mediaModel = mediaFactory(medias[i]);
+      const userMediaDom = mediaModel.getMediaCardDOM();
+      console.log(medias[i]);
+      photographersHeader.appendChild(userMediaDom);
+    }
+  }
+}
+// Récupèration la data des medias
 async function init() {
   const { media } = await getMedia();
   displayData(media);
 }
 init();
-
-/*
-// Séléction du photographe et affichage de son  :
-function fetchPhotographer(photographerId) {
-  fetch("./data/photographers.json").then(function (response) {
-    response.json().then(function (json) {
-      const mediaModel = mediaFactory(json.media);
-
-      // Data du photographe
-      const dataPhotographers = json.photographers.find((data) => data.id == photographerId);
-      const { city, country, id, name, portrait, price, tagline } = dataPhotographers;
-      // Medias du photographe
-      const dataMedias = json.media;
-      // Affichage des médias du photographe
-      for (let i = 0; i < dataMedias.length; i++) {
-        if (dataPhotographers.id == dataMedias[i].photographerId) {
-          // Implémentation du code !
-          console.log(mediaModel.getMediaCardDOM());
-          
-          console.log(
-            dataMedias[i].id,
-            dataMedias[i].photographerId,
-            dataMedias[i].title,
-            dataMedias[i].image,
-            dataMedias[i].likes,
-            dataMedias[i].date,
-            dataMedias[i].price
-          );
-        }
-      }
-    });
-  });
-}
-
-fetchPhotographer(photographerId);
-*/
