9baf43054069d1f5fd0545648d52edd156927430
Kevin Özkaraca <80677798+kevinozkaraca@users.noreply.github.com>
2023-03-27 04:27:20 +0300

    ajout de fonction

--- scripts/utils/tabindexPhotographer.js
+++ scripts/utils/tabindexPhotographer.js
@@ -4,6 +4,7 @@ function tabindexPhotographer() {
   const filter = document.querySelector("#filter");
   const tabindexPhotographerIMG = document.querySelectorAll("#imageAndVideoContainer");
   const tabindexLikes = document.querySelectorAll(".likeButton");
+  const imageAndVideoOnpage = document.querySelectorAll("#imageAndVideoOnpage");
   let tabindexArray = [];
   tabindexLogo.setAttribute("tabindex", "1");
   contactMe.setAttribute("tabindex", "2");
@@ -15,11 +16,18 @@ function tabindexPhotographer() {
   for (let i = 0; i <= tabindexPhotographerIMG.length - 1; i++) {
     tabindexArray.push(tabindexPhotographerIMG[i].setAttribute("tabindex", `4`));
     tabindexArray.push(tabindexLikes[i].setAttribute("tabindex", `4`));
-    tabindexLikes[i].addEventListener("focus", (element1) => {
+    tabindexLikes[i].addEventListener("focus", () => {
       document.addEventListener("keydown", (element2) => {
-
         if (element2.code == "Enter") {
           tabindexLikes[i].click();
+        }
+      })
+    })
+    tabindexPhotographerIMG[i].addEventListener("focus", () => {
+      document.addEventListener("keydown", (element1) => {
+        if (element1.code == "Enter") {
+          imageAndVideoOnpage[i].click();
+          console.log(tabindexPhotographerIMG[i]);
         }
       })
     })
